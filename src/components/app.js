0378c20930d237c028e611989ee68bb48624cfb7
Genaro Rocha <genarorg@gmail.com>
2016-11-07 23:10:17 -0800

    change message

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>It's aliiiiiiive puto!</div>
+      <div>It's aliiiiiiive!</div>
     );
   }
 }
